9f2f84dff65038900f85e621e0df79e30eb77915
Abhijit Nehe <abhijitnehe38@gmail.com>
2023-01-11 18:05:52 +0530

    setup gh-pages

--- src/Component/navbar.js
+++ src/Component/navbar.js
@@ -26,7 +26,7 @@ const Navbar = () => {
         <Link to='/'>
             <h1>Portfolio</h1>
         </Link>
-        <ul className= {click ? "nav-menu active" : "nav-menu"}>
+        <ul id = "unorder" className= {click ? "nav-menu active" : "nav-menu"}>
             <li>
                 <Link to="/">Home</Link>
             </li>
